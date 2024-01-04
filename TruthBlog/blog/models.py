bb4c1a798a70b6a4d2a640a7688a511fc24a2c99
RocketZwei <yangzhong217@outlook.com>
2024-01-04 10:33:17 +0100

    modify attributes of Post model

--- TruthBlog/blog/models.py
+++ TruthBlog/blog/models.py
@@ -1,18 +1,37 @@
-from datetime import timezone
+from datetime import datetime, timezone
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 
 
 class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     # By default, Django adds an auto-incrementing primary key field to each model.
     title = models.CharField(max_length=250)  # Translates into a VARCHAR column in the SQL database
-    slug = models.SlugFiled(max_length=250)   # A slug is a short label that contains only letters, numbers, underscores, or hyphens.
+    
+    slug = models.SlugField(max_length=250)   # A slug is a short label that contains only letters, numbers, underscores, or hyphens.
+    
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    
     body = models.TextField()  # Translates into a TEXT column in the SQL database.
-    publish = models.DateTimeField(default=timezone.now)  # Translates into a DATETIME column in the SQL database.
+    
+    publish = models.DateTimeField(default=datetime.now)  # Translates into a DATETIME column in the SQL database.
+    
     created = models.DateTimeField(auto_now_add=True)  # the date will be saved automatically when creating an object.
+    
     updated = models.DateTimeField(auto_now=True)  # the date will be saved automatically when saving an object.
+    
+    status = models.CharField(max_length=2,
+                          choices=Status.choices,
+                          default=Status.DRAFT)
+
 
     # This inner class defines metadata for the model. We use the ordering attribute to tell Django that it should sort results by the publish field. This ordering will apply by default for database queries when no specific order is provided in the query.
     class Meta:
